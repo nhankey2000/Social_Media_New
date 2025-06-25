3a56a63c842b715d46a2bae6bb877ca11988e2df
nhankey2000 <thanhnhan01216851830@gmail.com>
2025-06-25 10:52:49 +0700

    Initial commit

--- app/Filament/Resources/YouTubeAccountResource.php
+++ app/Filament/Resources/YouTubeAccountResource.php
@@ -77,6 +77,6 @@ class YouTubeAccountResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()->where('platform',3);
+        return parent::getEloquentQuery()->where('platform_id',3);
     }
 }
