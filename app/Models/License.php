fdd76b23c5c07620cc17efde2bb9090128f60132
nhankey2000 <thanhnhan01216851830@gmail.com>
2025-07-30 10:04:08 +0700

    Initial commit

--- app/Models/License.php
+++ app/Models/License.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; // ✅ Thêm dòng này
 use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
-    use HasFactory;
+    use HasFactory; // ✅ Sử dụng trait
 
     protected $fillable = [
         'machine_id',
